304b8428318ef17894bbac4201fc43b787edff6b
cutls <web-pro@cutls.com>
2020-08-02 11:08:59 +0900

    misktake params

--- index.ts
+++ index.ts
@@ -62,7 +62,7 @@ router
 		context.response.headers.set('Content-Type', `image/png`)
 		context.response.body = await Deno.readFile(`./static/${png}.png`)
 	})
-	.get('/webhook', async (context: ContextParams) => {
+	.get('/webhook/:secret', async (context: ContextParams) => {
 		const secret = context.params.secret
 		if(secret != config.secret) {
 			context.response.body = { success: false }
